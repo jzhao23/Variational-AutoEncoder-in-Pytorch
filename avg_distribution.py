3a15183565320374692e84c34691e1866185a31f
Jason Zhao <jasonjinzhao99@gmail.com>
2019-06-03 17:58:05 -0700

    Update avg_distribution.py

--- avg_distribution.py
+++ avg_distribution.py
@@ -136,22 +136,6 @@ def out_distribution_params():
 def compute_likelihood(x, mu, std_dev):
     return multivariate_normal.pdf(x, mu, std_dev)
 
-def norm_pdf_multivariate(x, mu, sigma):
-    size = len(x)
-    if size == len(mu) and (size, size) == sigma.shape:
-        det = np.linalg.det(sigma)
-        if det == 0:
-            raise NameError("The covariance matrix can't be singular")
-
-        norm_const = 1.0/ ( math.pow((2*pi),float(size)/2) * math.pow(det,1.0/2) )
-        x_mu = np.matrix(x - mu)
-        inv = sigma.I        
-        result = math.pow(math.e, -0.5 * (x_mu * inv * x_mu.T))
-        return norm_const * result
-    else:
-        raise NameError("The dimensions of the input don't match")
-
-
 avg_mu, avg_std = in_distribution_params()
 val_means, val_std_devs = in_distribution_val_params()
 out_means, out_std_devs = out_distribution_params()
