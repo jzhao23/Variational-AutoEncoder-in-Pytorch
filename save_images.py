1e8ccb969f365b3be57c7f8be3244491615ecd0f
Jason Zhao <jasonjinzhao99@gmail.com>
2019-06-05 21:42:26 -0700

    m

---
+++ save_images.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+from __future__ import division
+import argparse
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision
+from torchvision import models
+from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
+import matplotlib.pyplot as plt
+import time
+from tensorboardX import SummaryWriter
+from glob import glob
+from util import *
+import numpy as np
+from PIL import Image
+import warnings
+warnings.filterwarnings("ignore")
+
+path = '../OutDistribution/'
+kwargs = {'num_workers': 3, 'pin_memory': True} if is_cuda else {}
+
+simple_transform = transforms.Compose([transforms.Resize((224,224)),
+                                       transforms.Grayscale(num_output_channels=3),
+                                       transforms.ToTensor()])
+out = ImageFolder(path+'train/',simple_transform)
+out_data_gen = torch.utils.data.DataLoader(out,shuffle=True,batch_size=BATCH_SIZE,num_workers=kwargs['num_workers'])
+dataset_sizes = {'out':len(out_data_gen.dataset)}
+dataloaders = {'out':out_data_gen}
+
+count = 0
+for data in dataloaders['valid']:
+    count += 1
+    # get the inputs
+    inputs, _ = data
+
+    # wrap them in Variable
+    if torch.cuda.is_available():
+        inputs = Variable(inputs.cuda())
+    else:
+        inputs = Variable(inputs)
+    
+    if count == 1:
+        torchvision.utils.save_image(inputs.data, './imgs/POSTER_OUT_IMGS.jpg'.format(epoch), nrow=8, padding=2)
